e373819f6c32310dfc135321b1cc17d2385b2ecf
menuin <menuin@naver.com>
2021-02-21 21:00:16 +0900

    game over & restart 기능

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns=[
     path('', views.start_home , name="start_home"),
     path('<int:pk>/play', views.choose_word, name="play"),
+    path('<int:pk>/gameover',views.game_over,name="game_over"),
+    path('<int:pk>',views.restart,name="restart"), #<int:pk>/play로 하면 에러
 ]
--- core/views.py
+++ core/views.py
@@ -22,19 +22,38 @@ def choose_word(request,pk):
     wordList=Player.objects.get(pk=pk).words
     player=Player.objects.get(pk=pk)
 
-    if request.method=="GET": # correct answer
-        numOfWords=len(wordList)
-        choice=random.randint(1,numOfWords)
-        word=wordList[choice-1]
-        player.currentWord=word
-        player.save()
-        return render(request, 'core/play_home.html',{'pk':pk,'word':word})
+    if player.wrong<=6:
+        if request.method=="GET": # correct answer
+            numOfWords=len(wordList)
+            choice=random.randint(1,numOfWords)
+            word=wordList[choice-1]
+            player.currentWord=word
+            player.save()
+            return render(request, 'core/play_home.html',{'pk':pk,'word':word,'wrong':player.wrong})
 
-    else: # correct answer
-        if (request.POST['answer']==player.currentWord):
-            print(player.currentWord)
-            return redirect(reverse('play', kwargs={'pk':pk}))
-        else : # wrong answer
-            print("wrong")
-            return render(request, 'core/play_home.html',{'pk':pk,'word':player.currentWord})
+        else: 
+            # correct answer
+            if (request.POST['answer']==player.currentWord):
+                print(player.currentWord)
+                player.score=player.score+1
+                player.save()
+                return redirect(reverse('play', kwargs={'pk':pk}))
+            else : # wrong answer
+                print("wrong")
+                player.wrong=player.wrong+1
+                player.save()
+                return render(request, 'core/play_home.html',{'pk':pk,'word':player.currentWord,'wrong':player.wrong})
+    
+    else:
+        return redirect(reverse('game_over',kwargs={'pk':pk}))
 
+def game_over(request,pk):
+    player=Player.objects.get(pk=pk)
+    return render(request,'core/game_over.html',{'score':player.score,'player':player})
+
+def restart(request, pk):
+    player=Player.objects.get(pk=pk)
+    player.wrong=0
+    player.score=0
+    player.save()
+    return redirect(reverse('play',kwargs={'pk':pk}))
