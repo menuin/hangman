54c1a4bab99c180843f836f4903c579399c6d1db
김은민 <menuin@naver.com>
2021-03-26 01:00:12 +0900

    배포연습중

--- hangman/settings.py
+++ hangman/settings.py
@@ -25,22 +25,30 @@ from django.core.exceptions import ImproperlyConfigured
 
 secret_file = os.path.join(BASE_DIR,'secrets.json')
 
-with open(secret_file,'r') as f:
-    secrets = json.loads(f.read())
+# with open(secret_file,'r') as f:
+#     secrets = json.loads(f.read())
 
-def get_secret (setting, secrets=secrets):
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
+# # def get_secret (setting, secrets=secrets):
+# #     try:
+# #         return secrets[setting]
+# #     except KeyError:
+# #         error_msg = "Set the {} environment variable".format(setting)
+# #         raise ImproperlyConfigured(error_msg)
 
-SECRET_KEY=get_secret("SECRET_KEY")
+# # SECRET_KEY=get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG =os.getenv('DJANGO_DEBUG')!='FALSE'
 
-ALLOWED_HOSTS = []
+if DEBUG:
+    SECRET_KEY = "Hell@world!"
+else:
+    SECRET_KEY = os.getenv('SECRET_KEY')
+
+if DEBUG :
+    ALLOWED_HOSTS=['*']
+else:
+    ALLOWED_HOSTS = ['hangmanbymenuin.pythonanywhere.com']
 
 
 # Application definition
